084cf8bae3f3ff5975c69c3675e7b831ab67c695
stevet0pia <marcossteve166@gmail.com>
2026-09-17 10:53:58 +0300

    Add files via upload

---
+++ units in programming.c
@@ -0,0 +1,13 @@
+int main() {
+	printf("Name: Stephen Maina Njuguna. \n");
+	printf("Reg No: CT100/G/30665/26.  \n");
+	printf("Description: Units Covered in Programming. \n");
+	printf("Date: 17/9/2026  \n");
+	printf("Introduction to Programming. \n");
+	printf("Data Structures. \n");
+	printf("Computer Networks. \n");
+	printf("Database Systems. \n");
+	printf("Software Engineering. \n");
+	
+	return 0; //execution successful
+}
